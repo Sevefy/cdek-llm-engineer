c55a80b316fd73d2f50b8715d725bc43d0b3bd1c
seveCOS <sen.lofe@ya.ru>
2026-05-02 19:09:32 +0300

    изменена длина сообщения

--- app/schemas/chat.py
+++ app/schemas/chat.py
@@ -3,7 +3,7 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 class ChatRequestSchema(BaseModel):
-    text: str = Field(..., max_length=1024)
+    text: str = Field(..., max_length=4096)
 
 class ChatResponseSchema(BaseModel):
     response: str
